4d9576bbb1a1aea3f6cdbf51eabaecead303f893
martin <martinvondracek@gmail.com>
2015-12-31 10:20:42 +0100

    first pipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,14 @@ import {Component} from 'angular2/core';
 import {HeroListComponent} from './hero/hero-list.component';
 import {HeroFormComponent} from './hero-form/hero-form.component';
 import {HeroBirthday} from './pipes/hero-birthday.component';
+import {PowerBooster} from './pipes/power-booster.component';
+import {PowerBoostCalculator} from './pipes/power-boost-calculator.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.tpl.html',
-  directives: [HeroListComponent, HeroFormComponent, HeroBirthday]
+  directives: [HeroListComponent, HeroFormComponent, HeroBirthday, PowerBooster,
+  PowerBoostCalculator]
 })
 
 export class AppComponent {}
--- app/pipes/hero-birthday.component.ts
+++ app/pipes/hero-birthday.component.ts
@@ -1,8 +1,27 @@
 import {Component} from 'angular2/core'
+
 @Component({
   selector: 'hero-birthday',
-  template: `<p>The hero's birthday is {{ birthday | date }}</p>`
+  template: `
+    <p>The hero's birthday is {{ birthday | date:format }}</p>
+    <button (click)="toggleFormat()">Toggle format</button>
+    <br>
+    <p>
+      The chained hero's birthday is
+      {{ birthday | date:'fullDate' | uppercase}}
+    </p>
+    `
 })
 export class HeroBirthday {
   birthday = new Date(1988,3,15);
+
+  toggle = true;
+
+  get format() {
+    return this.toggle ? 'shortDate' : 'fullDate';
+  }
+
+  toggleFormat() {
+    this.toggle = !this.toggle;
+  }
 }
