7901da913dd0f4593473fa3e658e75eb8128d7e4
martin <martinvondracek@gmail.com>
2015-12-31 12:30:02 +0100

    basic routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
 import {HeroListComponent} from './hero/hero-list.component';
+import {CrisisListComponent} from './crisis/crisis-list.component';
 import {HeroFormComponent} from './hero-form/hero-form.component';
 import {HeroBirthday} from './pipes/hero-birthday.component';
 import {PowerBooster} from './pipes/power-booster.component';
@@ -10,13 +13,13 @@ import {HeroAsyncMessageComponent} from './pipes/hero-async-message.component';
   selector: 'my-app',
   templateUrl: 'app/app.component.tpl.html',
   directives: [
-    HeroListComponent,
-    HeroFormComponent,
-    HeroBirthday,
-    PowerBooster,
-    PowerBoostCalculator,
-    HeroAsyncMessageComponent
+    ROUTER_DIRECTIVES
   ]
 })
+
+@RouteConfig([
+  {path:'/crisis-center', name: 'CrisisCenter', component: CrisisListComponent},
+  {path:'/heroes', name: 'Heroes', component: HeroListComponent}
+])
 
 export class AppComponent {}
