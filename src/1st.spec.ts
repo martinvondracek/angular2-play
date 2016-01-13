41a20c592125ac9c8cb637c74379a1c1556a4ef1
martin <martinvondracek@gmail.com>
2016-01-13 16:09:11 +0100

    basic unit test

---
+++ src/1st.spec.ts
@@ -0,0 +1,9 @@
+describe('1st test', () => {
+
+  it('true is true', () => expect(true).toEqual(true));
+
+  it('null is not the same thing as undefined',
+    () => expect(null).not.toEqual(undefined)
+  );
+
+});
